8c925db42b53441599796538255c8d3d2e1ffb87
jeid12 <niyokwizerajd123@gmail.com>
2025-05-15 10:36:19 +0200

    update

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Wait for database to be ready
 echo "Waiting for PostgreSQL to be ready..."
-until nc -z -v -w30 $DB_HOST $DB_PORT
+until nc -z -v -w30 "$DB_HOST" "$DB_PORT"
 do
   echo "Waiting for database connection..."
   sleep 5
@@ -11,4 +11,6 @@ done
 # Laravel setup
 php artisan config:cache
 php artisan migrate --force
-php-fpm
+
+# Start Supervisor (will start PHP-FPM and NGINX)
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
